e7cfe26658d13d90c43a36216867718c9e4a98de
unknown <Koritheicequeen@gmail.com>
2024-07-10 11:09:36 -0400

    first commit

---
+++ src/main/java/mybotproject/ButtonClickListener.java
@@ -0,0 +1,21 @@
+package mybotproject;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ButtonClickListener extends ListenerAdapter {
+    public static List<Button> createButtons(List<String> EvolutionName) {
+        List<Button> buttons = new ArrayList<>();
+        for (String evolution : EvolutionName) {
+        buttons.add(Button.secondary(evolution, evolution));
+        }
+        return buttons;
+      
+    }
+}
